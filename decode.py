161e0a21b4d6a431eca024ad3eca15f81defe6ca
ao-spirogyra <lily.spirogyra@gmail.com>
2020-04-07 11:31:56 +0900

    書く

--- decode.py
+++ decode.py
@@ -1,8 +1,18 @@
 
+# decode vigenere cipher
+
 def calculateIndexOfCoincidence():
     for i in range(26)
         index_of_coincidence = F[i]*(F[i]-1) / N(N-1)
 
+def guessLengthOfKey(text)
+# use calculateIndexOfCoincidence()
+
+def decode(ciphered_text,length_of_key):
 
 
-def 
+if __name__ == "__main__"
+    ciphered_text = getTextFrom()
+    length_of_key = guessLengthOfKey(text)
+    plain_text = decode(ciphered_text,length_of_key)
+    print(plain_text)
