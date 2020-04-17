a67d58be5a22acb52b278f1cb248b0c82fe22e7b
ao-spirogyra <lily.spirogyra@gmail.com>
2020-04-17 09:41:56 +0900

    文字列を分割/結合する部分の実装

--- decode.py
+++ decode.py
@@ -1,4 +1,5 @@
 import string
+import math
 
 # decode vigenere cipher
 def count_alphabets(text,alphabets):
@@ -10,24 +11,35 @@ def calculateIndexOfCoincidence(text):
     N = len(text)
     alphabets = list(string.ascii_lowercase)
     F = count_alphabets(text,alphabets)
-    for i in num_alphabets
-        index_of_coincidence += F[i]*(F[i]-1)
-    return (index_of_coincidence/N(N-1))
+    index_of_coincidence = sum([F[i]*(F[i]-1) for i in alphabets])/N(N-1)
+    return index_of_coincidence
 
 def guessLengthOfKey(text)
     # use calculateIndexOfCoincidence()
 
 def splitTextToGroup(text,length_of_key)
     #split text into each group stepped by length of the key 
+    text_list = list(text)
+    for i in length_of_key
+        x = 0
+        for l in range(i,len(text),length_of_key)
+            group[i][x] = text_list[l]
+            x += 1
+    return group
 
-def unite():
-    
+def unite(group,length_of_key):
+    for i in len(group[0])
+        for l in length_of_key
+            plain_text = plain_text.append(group[l][i])
+    return plain_text
+
 
 def decode(ciphered_text,length_of_key):
-    for l in length_of_key
-        EachGroup[l] = splitTextToGroup(ciphered_text,l,length_of_key)
-        eachAnalyzedGroup = analyzeFrequency(EachGroup(l))
-    return unite(eachAnalyzedGroup)
+    group = splitTextToGroup(ciphered_text,length_of_key)
+    eachAnalyzedGroup = analyzeFrequency(group)
+    plain_text_list = unite(eachAnalyzedGroup,length_of_key)
+    plain_text = ''
+    return plain_text.join(plain_text_list)
     
 
 if __name__ == "__main__"
